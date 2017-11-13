9cf683303affe48431787f2f0ce4e1038559c78e
lazzytechnologies <lazycompany8882@gmail.com>
2017-11-13 14:19:22 +0800

    new

---
+++ includes/navigation.php
@@ -0,0 +1,49 @@
+<nav class="navbar navbar-default ">
+
+            <div class="container">
+                
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navigation">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php"><img src="assets/img/logo.png" alt=""></a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse yamm" id="navigation">
+                    
+                      <div class="button navbar-right">
+                          <button class="navbar-btn nav-button wow bounceInRight login" onclick=" window.open('index.php?source=loginandregister')" data-wow-delay="0.30s">POST PROPERTY NOW!</button>
+                          
+                      </div>
+                   
+                    <ul class="main-nav nav navbar-nav navbar-right">
+                      
+
+                       <li class="dropdown ymm-sw " data-wow-delay="0.1s">
+                            <a href="index.php?source=property-forsale" class="dropdown-toggle" data-toggle="ddropdown" data-hover="dropdown" data-delay="200">FOR SALE</a>
+                      </li>
+
+                      <li class="dropdown ymm-sw " data-wow-delay="0.1s">
+                             <a href="index.php?source=property-forrent" class="dropdown-toggle" data-toggle="ddropdown" data-hover="dropdown" data-delay="200">FOR RENT</a>
+                      </li>
+                   
+                    
+                      <li class="dropdown ymm-sw " data-wow-delay="0.1s">
+                            <a href="index.php?source=property-newdevelopment" class="dropdown-toggle" data-toggle="ddropdown" data-hover="dropdown" data-delay="200">NEW DEVELOPMENT</a>
+                           
+                      </li>
+                        
+                        
+
+                        
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+               
+            </div><!-- /.container-fluid -->
+        </nav>
+        <!-- End of nav bar -->
---
+++ index.php
@@ -0,0 +1,58 @@
+<?php include "includes/header.php" ?>
+<?php include "includes/navigation.php" ?>
+
+<?php 
+    
+    if (isset($_GET['source'])) {
+        
+        $source=$_GET['source'];
+    }
+    else
+    {
+        $source="";
+    }
+
+    switch ($source) 
+    {
+       
+        case 'property-forsale':
+             include "includes/property-main.php"; 
+            break;
+        case 'property-forrent':
+             include "includes/property-main.php"; 
+            break;
+        case 'property-foreclosure':
+             include "includes/property-main.php"; 
+            break;
+        case 'property-newdevelopment':
+             include "includes/property-main.php"; 
+            break;
+        case 'property-page':
+             include "includes/property-page.php"; 
+            break;
+        case 'loginandregister':
+             include "includes/login-register.php"; 
+            break;
+        case 'propertyadvertise':
+             include "includes/propertyadvertise.php"; 
+            break;
+        default:
+            include "includes/homepage-search.php";
+             include "includes/homepage-property.php"; 
+             include "includes/homepage-welcome.php"; 
+             include "includes/homepage-countarea.php"; 
+             include "includes/homepage-buyandsell.php";
+        break;
+    } 
+?>
+
+
+
+
+<?php include "includes/footer.php" ?>
+     
+      
+
+    
+
+      
